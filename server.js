4fa8ea541d473f42fce0b59611dd5d59f7355a51
rmkofficial <kmert0@outlook.com>
2024-08-07 20:47:42 +0300

    Add dotenv dependency for environment variables

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile');
 const { connectDB } = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 
@@ -15,6 +17,7 @@ connectDB();
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/profile', profileRoutes);
 
 // Server Setup
 const PORT = process.env.PORT || 5000;
